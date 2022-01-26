2fee58da215d855fa548acc98ec2c937661c0712
Marius Lopez <marius.lopez@utt.fr>
2022-01-26 11:59:28 +0100

    suppression diverses

--- TP01/Exercice2/main.cpp
+++ TP01/Exercice2/main.cpp
@@ -6,17 +6,7 @@
 
 int main(int argc, char *argv[])
 {
-    cv::Mat image;
-    cv::VideoCapture capture;
-    capture.open(0);
-    if(capture.isOpened()){
-        while(true){
-            capture >> image;
-            cv::imshow("test", image);
-            int c = cv::waitKey(10);//en ms, donc fps max -> 100
-            if(c == 27) break; //touche echap
-        }
-    }
+    //VOTRE CODE ICI
 
     return 0;
 }
